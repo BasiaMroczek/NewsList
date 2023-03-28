9fe78e3b6fc7f4226f44c9e7f0492eb055af390d
Barbara Mroczek <method.kontakt@gmail.com>
2023-03-28 19:08:15 +0200

    Sidebar content added

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,16 +1,20 @@
 import { useState } from 'react';
+import axios from 'axios';
 import "./style.css";
-
 
 const Sidebar = () => {
   const [activeCountry, setActiveCountry] = useState(null);
+  const [news, setNews] = useState([]);
 
-  const handleCountryClick = (countryCode) => {
+  const handleCountryClick = async (countryCode) => {
     setActiveCountry(countryCode);
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=d7d687aeef6c423e8158df1dbd6bda42`);
+    setNews(response.data.articles);
   };
 
   const handleClosePopup = () => {
     setActiveCountry(null);
+    setNews([]);
   };
 
   return (
@@ -21,65 +25,33 @@ const Sidebar = () => {
             <ul>
               <li>
                 <a href="#"
-                  data-country="at"
-                  onClick={() => handleCountryClick('Austrii')}>
+                  data-country="gr"
+                  onClick={() => handleCountryClick('gr')}>
                   <img
                     className="content__navigation--image"
-                    src="https://i.postimg.cc/RCfcJ8dy/Flaga-Austria.png"
+                    src="https://i.postimg.cc/hv27KFk8/Flaga-Grecja.png"
                   />
-                  Austria
-                </a></li>
-              <li>
-                <a href="#"
-                  data-country="bel"
-                  onClick={() => handleCountryClick('Belgii')}>
-                  <img
-                    className="content__navigation--image"
-                    src="https://i.postimg.cc/14hBw1V5/Flaga-Belgia.png"
-                  />
-                  Belgia
+                  Grecja
                 </a>
               </li>
               <li>
                 <a href="#"
-                  data-country="fi"
-                  onClick={() => handleCountryClick('Finlandii')}>
+                  data-country="ie"
+                  onClick={() => handleCountryClick('ie')}>
                   <img
                     className="content__navigation--image"
-                    src="https://i.postimg.cc/nzf1hw5J/Flaga-Finlandia.png"
+                    src="https://i.postimg.cc/L6r8L63Y/Flaga-Irlandia.png"
                   />
-                  Finlandia
-                </a>
-              </li>
-              <li>
-                <a href="#"
-                  data-country="fr"
-                  onClick={() => handleCountryClick('Francji')}>
-                  <img
-                    className="content__navigation--image"
-                    src="https://i.postimg.cc/XNmqWCw5/Flaga-Francja.png"
-                  />
-                  Francja
-                </a>
-              </li>
-              <li>
-                <a href="#"
-                  data-country="de"
-                  onClick={() => handleCountryClick('Niemiec')}>
-                  <img
-                    className="content__navigation--image"
-                    src="https://i.postimg.cc/9FQY09xr/Flaga-Niemcy.png"
-                  />
-                  Niemcy
+                  Irlandia
                 </a>
               </li>
               <li>
                 <a href="#"
                   data-country="no"
-                  onClick={() => handleCountryClick('Norwegii')}>
+                  onClick={() => handleCountryClick('no')}>
                   <img
                     className="content__navigation--image"
-                    src="https://i.postimg.cc/sXzprGQf/Flaga-Norwegia.png"
+                    src="https://i.postimg.cc/66PWx5Sj/Flaga-Norwegia.png"
                   />
                   Norwegia
                 </a>
@@ -87,12 +59,34 @@ const Sidebar = () => {
               <li>
                 <a href="#"
                   data-country="pl"
-                  onClick={() => handleCountryClick('Polski')}>
+                  onClick={() => handleCountryClick('pl')}>
                   <img
                     className="content__navigation--image"
                     src="https://i.postimg.cc/bNr1phJn/Flaga-Polska.png"
                   />
                   Polska
+                </a>
+              </li>
+              <li>
+                <a href="#"
+                  data-country="ru"
+                  onClick={() => handleCountryClick('ru')}>
+                  <img
+                    className="content__navigation--image"
+                    src="https://i.postimg.cc/jj85W6gK/Flaga-Rosja.png"
+                  />
+                  Rosja
+                </a>
+              </li>
+              <li>
+                <a href="#"
+                  data-country="ua"
+                  onClick={() => handleCountryClick('ua')}>
+                  <img
+                    className="content__navigation--image"
+                    src="https://i.postimg.cc/FRL490wT/Flaga-Ukraina.png"
+                  />
+                  Ukraina
                 </a>
               </li>
             </ul>
@@ -102,11 +96,19 @@ const Sidebar = () => {
       {activeCountry && (
         <div className="popup">
           <div>
-            <h2>Newsy z {activeCountry}</h2>
+            <h2>Newsy z {activeCountry.toUpperCase()}</h2>
             <button className="popup__close" onClick={handleClosePopup}>
               Zamknij okno
             </button>
-            <p className="popup__content">Tutaj umieść swoje newsy dla kraju {activeCountry}</p>
+            <ul className="popup__content">
+              {news.map((article, index) => (
+                <li key={index}>
+                  <p>{article.title}</p>
+                  <p>Autor: {article.author ? article.author : 'Nieznany autor'}</p>
+                  <a href={article.url} target="_blank" rel="noreferrer">Czytaj dalej</a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       )}
@@ -114,4 +116,4 @@ const Sidebar = () => {
   );
 };
 
-export default Sidebar
+export default Sidebar;
