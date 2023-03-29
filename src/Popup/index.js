7292a625faa825ff145dffd0045b8ce1517462df
Barbara Mroczek <method.kontakt@gmail.com>
2023-03-29 08:50:27 +0200

    Popup updated

--- src/Popup/index.js
+++ src/Popup/index.js
@@ -25,13 +25,15 @@ function Popup() {
             Jeszcze, ponieważ błyskawicznie się uczę i zawsze znajduję odpowiedzi na nurtujące mnie problemy.
           </p>
           <p>Wracając do zadania. Trudności, z którymi wydaje mi się, jak na laika poradziłam sobie poprawnie, sprawiły mi funkcje, dzięki którym pobierane są newsy. Uznaję to za trudność,
-            ponieważ nigdy wcześniej nie miałam z tym do czynienia. Nie ukrywam, że jestem bardzo podekscytowana faktem, że poczyniłam ten projekt samodzielnie.
+            ponieważ nigdy wcześniej nie miałam z tym do czynienia. Dodatkowo, klucz API, którego użyłam posiada blokadę, przez co nie jest możliwe odpalenie projektu z wygenerowanego przez
+            GG-Pages odnośnika. Wierzę w to, że po sklonowaniu mojego projektu i odpaleniu go lokalnie, wszystko będzie działać poprawnie. 
           </p>
-          <p>Przechodząc do radości związanych z wykonywaniem powierzonego mi zadania, największą było zwyczajnie kombinowanie co zrobić, aby kod działał. Popełniam błędy, co jest oczywistą
-            oczywistością biorąc pod uwagę fakt, że kompletnie nie mam doświadczenia, jednak satysfakcja z wykonania zadania jest największą nagrodą. Przyjemność sprawiało mi również stylowanie strony.
-            Zainspirowana Państwa stroną www stworzyłam prosty, czytelny styl, który jest odpowiedni do posiadanej zawartości.
+          <p>Przechodząc do radości związanych z wykonywaniem powierzonego mi zadania, największą było zwyczajnie kombinowanie co zrobić, aby kod działał. Nie raz, nie dwa, nie wiedziałam 
+            jak ugryźć dany problem, jednak po dłuższej chwili doszłam do odpowiedniego rozwiązania. Popełniam błędy, co jest oczywistą
+            oczywistością biorąc pod uwagę fakt, że kompletnie nie mam doświadczenia, jednak satysfakcja z wykonania zadania jest największą nagrodą. Przyjemność sprawiało mi również stylowanie.
+            Zainspirowana Państwa stroną www stworzyłam prosty, czytelny projekt, który jest odpowiedni do przedstawianej zawartości.
           </p>
-          <p>Mam nadzieję, że spełniłam Państwa oczekiwania i że będę miała możliwość zasilić Państwa szeregi.
+          <p>Mam nadzieję, że spełniłam Państwa oczekiwania i zachęciłam do poznania mnie osobiście.
           </p>
           <button className="popup__close-button" onClick={hidePopup}>
             Ukryj Popup
